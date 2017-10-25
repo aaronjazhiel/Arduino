aa65daeb94f36f2b7d31d974292a817373831117
Aaron Jazhiel Delgado Gonzalez <adelgago@everis.com>
2017-10-25 00:39:14 -0500

    Nombre:Aaron Jazhiel Descripcion: Practicas 1 y 2

---
+++ Hola_Mundo/Hola_Mundo.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // Con esta instruccion podemos settear la velocidad Serial 
+   Serial.begin(9600);
+}
+
+void loop() {
+  // Mediante esta instruccion 
+
+  
+  Serial.println("hola mundo");
+  delay(1000);
+
+ 
+    
+  }
+
+}
---
+++ configuracion_pines/configuracion_pines.ino
@@ -0,0 +1,21 @@
+void setup() {
+
+  //pintMode Sirve para configurar un pin digital
+  //parametros de entrada son el numero de pin y la constante
+  //INPUT = Para entradas digitales en general
+  //OUTPUT = Para salidas digitales en general
+
+   pinMode(2,INPUT_PULLUP);
+}
+
+void loop() {
+  
+
+  
+
+
+ 
+    
+  }
+
+}
