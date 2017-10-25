455babc7bbf176faf41ac74f2905753941d232f8
Aaron Jazhiel Delgado Gonzalez <adelgago@everis.com>
2017-10-25 01:08:13 -0500

    Se sube camnbios de configuracion del pin

--- configuracion_pines/configuracion_pines.ino
+++ configuracion_pines/configuracion_pines.ino
@@ -4,19 +4,21 @@
  * Descripción: Manejo de pines de entrada y salida
  */
 void setup() {
-
   //pintMode Sirve para configurar un pin digital
   //parametros de entrada son el numero de pin y la constante
   //INPUT = Para entradas digitales en general
   //OUTPUT = Para salidas digitales en general
-
-   pinMode(2,INPUT_PULLUP);
+   pinMode(2,INPUT_PULLUP); // Se establece como entrada
+   pinMode(3,INPUT_PULLUP);
+   Serial.begin(9600);// Inicializar la comunicacion serie
 }
 
 void loop() {
-  
 
- 
-  }
-
+// La funcion digitalRead sirve obtener el valor del pin con la constante cuando 
+// esta presionado obtenemos LOW(O).
+//LOW(0) = Bajo HIGH(1) = Alto
+   if(digitalRead(2) == LOW){ 
+      Serial.println("Me presionaste");
+   }
 }
